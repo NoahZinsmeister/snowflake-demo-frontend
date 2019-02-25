827068af684fa3accfba0916d098e455b898a95a
Noah Zinsmeister <noah@hydrogenplatform.com>
2019-02-25 12:18:21 -0500

    add nonce logic

--- lambda-src/provider.js
+++ lambda-src/provider.js
@@ -3,10 +3,12 @@ import { providers, Wallet } from 'ethers'
 const provider = new providers.JsonRpcProvider(process.env.REACT_APP_INFURA_URL)
 const wallet = new Wallet(process.env.REACT_APP_PRIVATE_KEY, provider)
 
-export async function handler (event, context) {
+export async function handler (event) {
   try {
     const { to, transactionData } = JSON.parse(event.body)
-    const transaction = await wallet.sendTransaction({ to, data: transactionData })
+    const nonce = await provider.getTransactionCount(wallet.address, 'pending')
+
+    const transaction = await wallet.sendTransaction({ to, data: transactionData, nonce })
 
     return {
       statusCode: 200,
